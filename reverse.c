621a6467600fbdf7ce7bb46892ccf0350ea90c77
aishwarya52 <aishusara52@gmail.com>
2017-07-20 14:26:51 +0530

    reverse.c

---
+++ reverse.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+  void main()
+   {
+       char n[50],r;
+       printf("enter the string");
+       scanf("%d",&n);
+       
+       rev=strrev(n);
+       printf("The reverse string: %c",&rev);
+   }
