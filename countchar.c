bb1596d1b9847751bdfdb8aeb798847d9a4a5460
aishwarya52 <aishusara52@gmail.com>
2017-07-24 13:22:59 +0530

    Create countchar.c

---
+++ countchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+main()
+{
+  char str[100];
+   int i=0,l=0;
+   gets(str);
+   for(i=0; str[i]!='\0';i++)
+   {
+       if((str[i]>='A' && str[i]<='Z') || (str[i]>='a' && str[i]<='z'))
+      l=l+1;
+   }
+   printf("The number of characters in the string are %d\n",l);
+    
+    return 0;
+}
